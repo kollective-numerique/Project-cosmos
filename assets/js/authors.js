f66f567630231c2648fd3fc6943822425879e18a
seleshabani <seleshabani4@gmail.com>
2022-12-30 16:36:17 +0100

    fix/authors-by-sele : affichage de la liste des auteurs à travers une fonction qui est appelé à chaque tour de boucle puis inseré dans la div authorsList qui est censé tous le contenir

--- assets/js/authors.js
+++ assets/js/authors.js
@@ -4,23 +4,36 @@ let openBtn = document.getElementById("openBtn");
 openBtn.onclick = openNav;
 
 function openNav() {
-  sidenav.classList.toggle("active");
+    sidenav.classList.toggle("active");
 }
 
 /* RECUPERATION DES DATAS DE L'API */
 
 
-for(var id = 1; id<6; id++){
-  fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
-  
-  .then((res)=> res.json())
-  .then((result)=>{
-    console.log(result)
-    for(i=1; i< result.length; i++){
-      var div = document.getElementById('text')
-      div;
-    } 
-    let printElement = result.title
-    document.getElementById('article').innerHTML += printElement
-  }) 
-} 
+for (var id = 1; id < 6; id++) {
+    fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+        .then((res) => res.json())
+        .then((result) => {
+            console.log(result)
+                /*   for (i = 1; i < result.length; i++) {
+                      var div = document.getElementById('text')
+                      div;
+                  } */
+                /* let printElement = result.title */
+            document.querySelector('.authorsList').innerHTML += display_author(result.thumbnailUrl, result.title);
+        })
+}
+
+let display_author = function(thumbnailUrl, title) {
+    return (
+        `<div class="authorsBox">
+          <div class="top">
+            <img src="${thumbnailUrl}" alt="" id="image" />
+          </div>
+          <div id="text">
+            <span id="article">${title}</span>
+          </div>
+     </div>
+    `
+    )
+}
