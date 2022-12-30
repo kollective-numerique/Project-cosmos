34d63b949c336529006c8b9c03cc2a9d162702f1
Mister-no-body <leonsnguela08@gmail.com>
2022-12-30 13:05:43 +0100

    premier push de la récupération des datas via l'API

--- assets/js/authors.js
+++ assets/js/authors.js
@@ -6,3 +6,21 @@ openBtn.onclick = openNav;
 function openNav() {
   sidenav.classList.toggle("active");
 }
+
+/* RECUPERATION DES DATAS DE L'API */
+
+
+for(var id = 1; id<6; id++){
+  fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+  
+  .then((res)=> res.json())
+  .then((result)=>{
+    console.log(result)
+    for(i=1; i< result.length; i++){
+      var div = document.getElementById('text')
+      div;
+    } 
+    let printElement = result.title
+    document.getElementById('article').innerHTML += printElement
+  }) 
+} 
